40fbf7359037d1cff146f25cd3349e7c7550d4cd
Piero Rucci <105915583+ruccipiero@users.noreply.github.com>
2023-03-13 16:49:19 +0100

    Webcam access code

---
+++ webcam.py
@@ -0,0 +1,28 @@
+
+import rospy
+from std_msgs.msg import String, UInt32
+from sensor_msgs.msg import Image
+import cv2
+import numpy 
+
+
+
+
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("preview")
